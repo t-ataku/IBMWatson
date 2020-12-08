7b5d55ccecbd8c978260bb7f85bcb168d837633f
Toshimi Ataku <ATAKU@jp.ibm.com>
2020-12-08 21:51:39 +0900

    add function to list supported voices

--- Speak.sh
+++ Speak.sh
@@ -10,16 +10,24 @@
 # }
 
 URLBASE=http://localhost:8443/
-URLBASE=https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/9df76f31-1f98-40d3-890e-c0a7cbda962e/v1/synthesize
+URLBASE=https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/ac07d247-9ff6-4714-a718-ca1d650c9aed
+URLDO=v1/synthesize
 #https://gateway-lon.watsonplatform.net/text-to-speech/api/v1/synthesize
+URLLIST=v1/voices
 
 function getvoicename {
     case $1 in
-	en-US)
+	en)
 		echo "en-US_LisaVoice";
 		;;
-	ja-JP)
+	ja)
 		echo "ja-JP_EmiVoice";
+		;;
+	ko)
+		echo "ko-KR_YoungmiVoice";
+		;;
+	zh)
+		echo "zh-CN_LiNaVoice";
 		;;
 	*)
 	    exit 1;;
@@ -27,7 +35,7 @@ function getvoicename {
     return 0
 }
 
-while getopts "l:o:" opt $@; do
+while getopts "l:o:v" opt $@; do
     echo "\$opt=" $opt; echo "OPTARG=" $OPTARG
     echo "OPTIND=" $OPTIND
     case $opt in
@@ -37,6 +45,10 @@ while getopts "l:o:" opt $@; do
 	"o")
 	    ofile=$OPTARG;
 	    ;;
+        "v")
+            curl -u "apikey:$(cat sppwd)" ${URLBASE}/${URLLIST};
+            exit 0;
+            ;;
 	*)
 	    echo "Undefined option was specified: [$OPTIND]" >&2
 	    ;;
@@ -70,4 +82,4 @@ END {
 	print "}"
 }
 ' > data.dat
-curl -v -X POST -u "apikey:$(cat sppwd)" --header "Content-Type: application/json" --header "Accept: audio/wav" --data @data.dat --output $ofile ${URLBASE}'?'voice=$voice
+curl -v -X POST -u "apikey:$(cat sppwd)" --header "Content-Type: application/json" --header "Accept: audio/wav" --data @data.dat --output $ofile ${URLBASE}/${URLDO}'?'voice=$voice
